42f9babb1342a6078d9e0198e3ef9e2e2115a256
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-11 20:19:34 +0200

    error habdling - status codes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -30,14 +30,24 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
 
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return{"detail": post}
+
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
+
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = f"post with id: {id} was not found")
     return {"post_detail": post}
+
