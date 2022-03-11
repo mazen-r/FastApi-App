8284b2f011862297d930bc2bffb4da0d3757c844
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-11 19:45:14 +0200

    create-update posts

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{'title': 'title of post 1', 'content':'content of post one',
+            'id':1
+}]
+
+def find_post(id):
+    for i in my_posts:
+        if i['id'] == id:
+            return i
+
 @app.get("/")
-async def root():
+def root():
     return {"message":"Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print (payLoad)
-    return {"new_post": f"title {payLoad['title']} content: ({payLoad['content']})"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
