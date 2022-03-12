d16d8fb80647fcf98dafdcd1cd2c93077a176bf7
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-12 21:07:25 +0200

    small change

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -15,14 +16,19 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-try:
-    conn = psycopg2.connect(host='localhost', database='fastapi',
-    user='postgres', password='admin', cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print ("Database connection was successful")
-except Exception as error:
-    print ("Connecting to database failed")
-    print ("Error: ", error)
+while True:
+
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi',
+        user='postgres', password='admin', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print ("Database connection was successful")
+        break
+
+    except Exception as error:
+        print ("Connecting to database failed")
+        print ("Error: ", error)
+        time.sleep(2)
 
 
 
