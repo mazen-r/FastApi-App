0a35328e0851e471f224a9230b8f674a8eaf7f41
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-14 21:39:48 +0200

    creating user

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -9,10 +9,20 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
-
 class Post(PostBase):
     id: int
     created_at: datetime
 
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at:  datetime
     class Config:
         orm_mode = True
